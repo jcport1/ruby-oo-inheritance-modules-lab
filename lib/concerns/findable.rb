824c61d739633aa13455884981b17a76d4836292
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-11-13 00:05:15 -0500

    commit tree 335bdb1a6d32334dd1212a381f0aff39dfd4e86c

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,15 @@
+
+module Findable
+
+
+
+    def find_by_name(name)
+        all.detect{|a| a.name == name}
+      end
+
+    #   def find_by_name(name)
+    #     all.detect{|a| a.name == name}
+    #   end
+
+
+end 
---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,14 @@
+module Memorable
+
+    #self not necessary for class methods in module
+
+def reset_all
+    all.clear
+  end
+
+  def count
+    all.count
+  end
+
+
+end
